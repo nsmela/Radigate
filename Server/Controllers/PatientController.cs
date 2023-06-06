d35e656c0a6843382dc79bd3d4fabf64be631437
Nathan Smela <nsmela@hotmail.com>
2023-06-06 11:16:10 -0700

    patient seeding

--- Server/Controllers/PatientController.cs
+++ Server/Controllers/PatientController.cs
@@ -5,19 +5,10 @@ namespace Radigate.Server.Controllers {
     [Route("api/[controller]")]
     [ApiController]
     public class PatientController : ControllerBase {
-        private static List<Patient> Patients = new List<Patient>{
-            new Patient {Id= 0, LastName = "Carey", FirstName="Drew"},
-            new Patient {Id= 1, LastName = "Stiles", FirstName="Ryan"},
-            new Patient {Id= 2, LastName = "Proops", FirstName="Greg"},
-            new Patient {Id= 3, LastName = "Mocharie", FirstName="Colin"},
-            new Patient {Id= 4, LastName = "Bradey", FirstName="Wayne"},
-            new Patient {Id= 5, LastName = "Taylor", FirstName="Aishia"},
-            new Patient {Id= 5, LastName = "Owen", FirstName="Clive"}
-        };
 
         [HttpGet]
         public async Task<ActionResult<List<Patient>>> GetPatient() {
-            return Ok(Patients);
+            return Ok();
         }
     }
 }
--- Server/Data/PatientDataContext.cs
+++ Server/Data/PatientDataContext.cs
@@ -10,6 +10,18 @@
             optionsBuilder.UseSqlite(Configuration.GetConnectionString("SQLLiteConnection"));
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<Patient>().HasData(
+                new Patient { Id = 1, LastName = "Stiles", FirstName = "Ryan" },
+                new Patient { Id = 2, LastName = "Proops", FirstName = "Greg" },
+                new Patient { Id = 3, LastName = "Mocharie", FirstName = "Colin" },
+                new Patient { Id = 4, LastName = "Bradey", FirstName = "Wayne" },
+                new Patient { Id = 5, LastName = "Taylor", FirstName = "Aishia" },
+                new Patient { Id = 6, LastName = "Owen", FirstName = "Clive" },
+                new Patient { Id = 7, LastName = "Carey", FirstName = "Drew" }
+            );
+        }
+
         public DbSet<Patient> Patients { get; set; }
     }
 }
