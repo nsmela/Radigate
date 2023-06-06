d448d819d2bbbe6aa7284396ec369a9c78cfca46
Nathan Smela <nsmela@hotmail.com>
2023-06-06 11:23:16 -0700

    able to call patients from sql lite

--- Server/Controllers/PatientController.cs
+++ Server/Controllers/PatientController.cs
@@ -1,14 +1,21 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Radigate.Server.Data;
 
 namespace Radigate.Server.Controllers {
     [Route("api/[controller]")]
     [ApiController]
     public class PatientController : ControllerBase {
+        private readonly PatientDataContext _context;
+
+        public PatientController(PatientDataContext context) {
+            _context = context;
+        }
 
         [HttpGet]
         public async Task<ActionResult<List<Patient>>> GetPatient() {
-            return Ok();
+            var patients = await _context.Patients.ToListAsync();
+            return Ok(patients);
         }
     }
 }
