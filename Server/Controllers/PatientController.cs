3013aafa3147fb5a77e9ba2e30afd23c3dfb6144
Nathan Smela <nsmela@hotmail.com>
2023-06-06 07:33:25 -0700

    Add project files.

---
+++ Server/Controllers/PatientController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Radigate.Server.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientController : ControllerBase {
+        private static List<Patient> Patients = new List<Patient>{
+            new Patient {Id= 0, LastName = "Carey", FirstName="Drew"},
+            new Patient {Id= 1, LastName = "Stiles", FirstName="Ryan"},
+            new Patient {Id= 2, LastName = "Proops", FirstName="Greg"},
+            new Patient {Id= 3, LastName = "Mocharie", FirstName="Colin"},
+            new Patient {Id= 4, LastName = "Bradey", FirstName="Wayne"},
+            new Patient {Id= 5, LastName = "Taylor", FirstName="Aishia"}
+        };
+
+        [HttpGet]
+        public async Task<ActionResult<List<Patient>>> GetPatient() {
+            return Ok(Patients);
+        }
+    }
+}
---
+++ Shared/Patient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radigate.Shared {
+    public class Patient {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+    }
+}
