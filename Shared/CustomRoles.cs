9872ba045ddd83ffc726769f5474cc0dfdaaed8d
Nathan Smela <nsmela@hotmail.com>
2023-06-20 11:24:51 -0700

    adding admin security

--- Shared/CustomRoles.cs
+++ Shared/CustomRoles.cs
@@ -1,4 +1,4 @@
-﻿namespace Radigate.Server.Users {
+﻿namespace Radigate.Shared {
     // https://stackoverflow.com/questions/700166/allow-multiple-roles-to-access-controller-action
     public static class CustomRoles {
         public const string Admin = "Admin";
